4b30d6d65764858406f282ae2c9e152539833751
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-01 13:20:21 -0700

    created and set time variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
-var questionList = document.querySelector("questions");
+var timeDisplay = document.querySelector("#timeDisplay")
+var timer = document.querySelector("starter");
+
+var timeHolder = 0;
+var startingPoint = 60;
+var wrongPenalty = 5;
+
+
+// trigger for timer to appear and begin countdown
+timer.addEventListener("click", function () {
+    if (timeHolder === 0) {
+        timeHolder = setTime (function () {
+            startingPoint--;
+            timeDisplay.textContent = "Remaining Time: " + startingPoint;
+            if (startingPoint <= 0) {
+                clearInterval(timeHolder);
+                timeDisplay.textContent = "Sorry but time is up";
+            }
+        }, 1000);
+    }
+})
+
+
+var questionList = document.querySelector("#questions");
 
 function questions() {
     questionList.innerHTML = "Test";
