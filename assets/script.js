48cb633d2410181c6ae897e3b9e35742f16af9bd
jonnyboy808 <125600257+jonnyboy808@users.noreply.github.com>
2023-04-02 10:54:54 -0700

    added input box to completed quiz

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,7 @@ var createList = document.createElement("ul");
 
 // variables for timer
 var timeHolder = 0;
-var startingPoint = 60;
+var startingPoint = 2;
 var wrongPenalty = 5;
 var score = 0;
 var questionOptions = 0;
@@ -18,7 +18,7 @@ timer.addEventListener("click", function () {
             timeDisplay.textContent = "Remaining Time: " + startingPoint;
             if (startingPoint <= 0) {
                 clearInterval(timeHolder);
-                complete();
+                end();
                 timeDisplay.textContent = "Sorry but time is up";
             }
         }, 1000);
@@ -42,6 +42,8 @@ function display(questionOptions) {
         listOptions.addEventListener("click", (compare));
     })
 }
+
+
 
 
 function compare(event) {
@@ -93,7 +95,51 @@ function end() {
         addP.textContent = "Your final score is: " + timeRemaining;
         card.appendChild(addP2);
     }
-}
+
+    var textBox = document.createElement("label");
+    textBox.setAttribute("id", "textBox");
+    textBox,textContent = "Type your initials: ";
+    card.appendChild(textBox);
+
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+    card.appendChild(createInput);
+
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+    card.appendChild(createSubmit);
+
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+            console.log("No Value");
+        } else {
+            var totalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(totalScore);
+            var previousScores = localStorage.getItem("previousScore");
+            if (previousScores ===  null) {
+                previousScores = [];
+            } else {
+                previousScores = JSON.parse(previousScores);
+            }
+            previousScores.push(totalScore);
+            var newScore = JSON.stringify(previousScores);
+            localStorage.setItem("previousScores", newScore);
+            window.location.replace("./")
+        }
+    })
+
+
+
+} 
 
 
 
